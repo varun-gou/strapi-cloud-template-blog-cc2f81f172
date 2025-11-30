025029f9c9028fa0c97404f1493416f242cdc6c3
varungoud <113126260+varun-gou@users.noreply.github.com>
2025-11-30 22:33:02 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -14,4 +14,9 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  transfer:{
+    token:{
+      salt:env('TRANSFER_TOKEN_SALT'),
+    }
+  }
 });
